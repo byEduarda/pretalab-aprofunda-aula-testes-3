d7a5d940b2f93f538c15d7c641b222b1588593a0
Eduarda Albuquerque <albuquerque.zxl@gmail.com>
2025-07-01 19:50:12 -0300

    testes para POST /users com criação bem-sucedida e validação de email duplicado

--- src/testes/integration/CreateUserIntegrationTest.spec.ts
+++ src/testes/integration/CreateUserIntegrationTest.spec.ts
@@ -1,8 +1,37 @@
 
 import request from 'supertest';
 
-import app frpm '../../infra/server/server';
+import app from '../../infra/server/server';
 
-describe('POST / users', () => {
-    
-})
+describe("POST / users", () => {
+
+    it("deve criar um novo usuário com sucesso", async () => {
+        const response = await request(app).post("/users").send({
+            name: "Dandara Oliveira da Silva",
+            login: "dandara1995",
+            email: "dandara@example.com",
+            password: "123456"
+        });
+
+        expect(response.status).toBe(201)
+    });
+
+    it("deve retornar erro em caso de email duplicado", async () => {
+         await request(app).post("/users").send({
+            name: "Dandara Oliveira da Silva",
+            login: "dandara1995",
+            email: "dandara@example.com",
+            password: "123456"
+        });
+        const response = await request(app).post("/users").send({
+            name: "Bianca Santana",
+            login: "bianca1995",
+            email: "dandara@example.com",
+            password: "1xjsjs"
+        });
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('error', 'usuário já existe com esse email')
+
+
+    });    
+});
