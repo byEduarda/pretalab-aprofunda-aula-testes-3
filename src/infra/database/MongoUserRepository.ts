6ccb2dbd03505c3cadae7d41f162d4dd402cf1aa
byEduarda <albuquerque.zxl@gmail.com>
2025-07-07 20:44:35 -0300

    Create MongoUserRepository.ts

---
+++ src/infra/database/MongoUserRepository.ts
@@ -0,0 +1,34 @@
+import { User } from '../../core/entities/User';
+import { UserRepository } from '../../core/repositories/UserRepository';
+import { userModel} from '../database/mongooseUserModel';
+
+export class MongoUserRepository implements UserRepository {
+    private toEntity(doc: any): User {
+        return new User(doc.id, doc.name, doc.login, doc.email, doc.password)
+    }
+
+    async save(user: User){
+        await userModel.create(user)
+    }
+
+    async findByEmail(email: string){
+
+        const doc = await userModel.findOne({ email });
+
+        return doc ? this.toEntity(doc) : null
+
+    }
+
+    async findById(id: string){
+
+        const doc = await userModel.findOne({ id });
+
+        return doc ? this.toEntity(doc) : null
+
+    }
+
+    async update(user: User) {
+        const doc = await userModel.findByIdAndUpdate;
+    }
+   
+}
