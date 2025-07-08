0c8573fdbc918f4f0b891476f28b8040a1d1243c
byEduarda <albuquerque.zxl@gmail.com>
2025-07-08 20:57:56 -0300

    .

--- src/infra/database/MongoUserRepository.ts
+++ src/infra/database/MongoUserRepository.ts
@@ -4,32 +4,33 @@ import { userModel } from '../database/mongooseUserModel';
 
 export class MongoUserRepository implements UserRepository {
     private toEntity(doc: any): User {
-        return new User(doc.id, doc.name, doc.login, doc.email, doc.password)
+        return new User(doc.id, doc.name, doc.login, doc.email, doc.password, doc._id.toString());
     }
 
-    async save(user: User) {
-        await userModel.create(user)
+
+    async save(user: User): Promise<User> {
+       const saveUser = await this.UserRepository.save(user)
+
+       return saveUser
     }
 
     async findByEmail(email: string) {
-
         const doc = await userModel.findOne({ email });
 
         return doc ? this.toEntity(doc) : null
-
     }
 
     async findById(id: string) {
-
-        const doc = await userModel.findOne({ id });
+        const doc = await userModel.findOne({ _id: id });
 
         return doc ? this.toEntity(doc) : null
-
     }
+
 
     async update(user: User) {
         const doc = await userModel.findByIdAndUpdate(
-            user.id, {
+            user.id,
+            {
                 name: user.name,
                 login: user.login,
                 email: user.email,
